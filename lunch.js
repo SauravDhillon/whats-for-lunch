672bf29fa879eb4b6b134e71b8f9ce85cdbb6b4f
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-11 23:56:05 -0500

    Some important context here

--- lunch.js
+++ lunch.js
@@ -14,7 +14,20 @@
  */
 
 const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
+  function whatToDoForLunch(hungry, availableTime){
+    if(hungry === true && availableTime < 20){
+      console.log("Pick up a snack or grab something you have ready at home");
+    }
+   else if(hungry === true && availableTime >= 20 && availableTime <= 30){
+     console.log("You deserve a break and should take time to cook a tasty meal");
+   } else if(hungry === true && availableTime > 30){
+     console.log("This is an intense program afterall and you should probably reconsider");
+   }else{
+     console.log("Wait until you are hungry");
+   }
+    console.log("hungry is", hungry);
+    console.log("availableTime is", availableTime);
+  }
 }
 
 
